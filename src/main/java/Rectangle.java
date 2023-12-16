4eda5355371b0d907abed3afaf2979b3245851c4
ahincho <ahincho@unsa.edu.pe>
2023-12-16 18:57:00 -0500

    Rectangle Class

---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,38 @@
+public class Rectangle {
+    // Attributes
+    private int height;
+    private int width;
+    // Empty Constructor
+    public Rectangle() {
+        this.height = 0;
+        this.width = 0;
+    }
+    // All Args Constructor
+    public Rectangle(int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
+    // Getters
+    public int getHeight() {
+        return this.height;
+    }
+    public int getWidth() {
+        return this.width;
+    }
+    // Get Rectangle Area
+    public int getArea() {
+        return this.height * this.width;
+    }
+    // Setters
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    public void setWidth(int width) {
+        this.width = width;
+    }
+    // ToString Method
+    @Override
+    public String toString() {
+        return "Rectangle: [height = " + this.height + ", width = " + this.width + ", area = " + getArea() + "]";
+    }
+}
